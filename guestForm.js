1aa0b888227ff3a03e9e0c00214a9b0fe5bb9dfe
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2023-12-15 02:02:42 +0700

    Update guestForm.js

--- guestForm.js
+++ guestForm.js
@@ -10,75 +10,66 @@ function guestForm() {
 
   // 1. register event for searching and adding
   function registerEventHandling() {
-    const searchInput = document.querySelector("#search-input")
+    const searchInput = document.querySelector('#search-input')
     const addButton = document.querySelector('#add-guest-btn')
-
-    searchInput.addEventListener('keyup', searchGuest)
-
-    addButton.addEventListener('click', addGuest)
+    searchInput.addEventListener('keyup',searchGuest)
+    addButton.addEventListener('click',addGuest)
   }
 
   // 2. Function to display one guest in the display area
   function displayGuest(guestItem) {
-    const divParent = document.querySelector('#display-area');
+    const divDisplay = document.querySelector('#display-area')
     const newDiv = document.createElement('div')
     const newSpan1 = document.createElement('span')
     const newSpan2 = document.createElement('span')
-    newSpan1.textContent = `${guestItem.firstname} ${guestItem.lastname}`
-    newSpan2.setAttribute('class', 'remove-icon')
-    newSpan2.setAttribute('id', `${guestItem.firstname}-${guestItem.lastname}`)
-    newSpan2.setAttribute('style', 'cursor:pointer;color:red')
-    newSpan2.textContent = ' [X]'
+
+    newSpan1.innerHTML = `${guestItem.firstname} ${guestItem.lastname}`
+    newSpan2.setAttribute('class','remove-icon')
+    newSpan2.setAttribute('id',`${guestItem.firstname}-${guestItem.lastname}`)
+    newSpan2.setAttribute('style','cursor:pointer;color:red')
+    newSpan2.innerHTML = " [X]"
     newSpan2.addEventListener('click', removeGuest)
 
     newDiv.appendChild(newSpan1)
     newDiv.appendChild(newSpan2)
-    divParent.appendChild(newDiv)
+    divDisplay.appendChild(newDiv)
   }
 
   // 3. Function to display all existing guests in the display area
   function displayGuests(guestResult) {
-    const divParent = document.querySelector('#display-area');
-    // Clear all previous children within div
-    divParent.innerHTML = '';
-
-    guestResult.forEach(guest => {
-      displayGuest(guest)
-    });
-
+    const divDisplay = document.querySelector('#display-area')
+    divDisplay.innerHTML = ''
+    guestResult.forEach((guestItem) => displayGuest(guestItem))
   }
 
   // 4. Function to search and display matching guests
   function searchGuest(event) {
-    const matchingGuests = guests.searchGuests(event.target.value);
-    displayGuests(matchingGuests);
+    const searchInput = document.querySelector('#search-input')
+    const findGuest = guests.searchGuests(searchInput.value)
+    displayGuests(findGuest)
   } 
 
   // 5. Function to add a new guest
   function addGuest() {
-    const fnameInput = document.querySelector('#firstname-input') 
+    const fnameInput = document.querySelector('#firstname-input')
     const lnameInput = document.querySelector('#lastname-input')
-    
-    const newGuestObject = guests.addNewGuest(fnameInput.value,lnameInput.value)
+   
+    guests.addNewGuest(fnameInput.value,lnameInput.value)
+    displayGuest({firstname: fnameInput.value, lastname: lnameInput.value})
 
-    displayGuest({ firstname: fnameInput.value, lastname: lnameInput.value })
-    
-    fnameInput.value = "";
-    lnameInput.value = "";
-  
+    fnameInput.value = ''
+    lnameInput.value = ''
   }
 
   // 6. Function to remove a guest
   function removeGuest(event) {
-    const removeValue = event.target.id // Kenny-Poppy
-    const splitValue = removeValue.split('-') // [Kenny, Poppy]
-    const fname = splitValue[0] // Kenny  
-    const lname = splitValue[1] // Poppy
-    const deleteGuest = {firstname: `${fname}`, lastname:`${lname}`}
-
+    const eventId = event.target.id
+    const splitName = eventId.split('-')
+    const fname = splitName[0]
+    const lname = splitName[1]
+    const deleteGuest =  {firstname: fname, lastname: lname}
     guests.removeGuest(deleteGuest)
-    // ขึ้นไปลบ Div ข้างบนใน Element
-    const removeDiv = event.target.parentElement
+    const removeDiv = event.target.parentElement // event.target == span , event.target.parentElement == div 
     removeDiv.remove()
   }
 
